4abdc21ec50c8f8a18f7b2e52ccfab368ee716f3
aniket.g <aniket.g@DESKTOP-PN5LUQS.ctlep.local>
2020-09-24 18:36:04 +0530

    Updated path

--- Config/StartUpScript.bat
+++ Config/StartUpScript.bat
@@ -1,11 +1,5 @@
 title StartUpScript
-set automationTestScriptsLocation=D:\POC\MOH\ASIS\AutomationTesting\Development\Source\Scripts
-set TestNGLocation=D:\POC\MOH\ASIS\AutomationTesting\Development\Config
-set TestNG=testng.xml
-
-cd %automationTestScriptsLocation%
-set classpath=%automationTestScriptsLocation%\bin;%automationTestScriptsLocation%\Library\*
-java org.testng.TestNG %TestNGLocation%\%TestNG%
+mvn clean test -DsuiteXmlFile=D:\Roger\Development\Source\Scripts\pom.xml
 pause
 
 
